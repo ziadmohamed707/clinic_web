7450f903960b757db80a0d65197635b6f4d72f8d
Ziad Mohamed <Ziadm707.zm@gmail.com>
2025-11-16 10:53:13 +0200

    update physioone web build

---
+++ physioone/geolocator_web_options.js
@@ -0,0 +1,10 @@
+// This file is used to configure the geolocator web package.
+//
+// It is not required to use this file, but it is recommended to customize
+// the behavior of the geolocator web package.
+
+// ignore_for_file: prefer_const_declarations
+
+var geolocator_web_options = {
+  enableHighAccuracy: true,
+};
